0a30633b2e57baa7ea2b309903987d4cdfc4d63d
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-02-09 18:14:17 +0900

    :white_check_mark: Solve. 1330번

--- src/Problem1330.java
+++ src/Problem1330.java
@@ -1,2 +1,28 @@
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem1330 {
+
+    public static void main(String[] args) {
+        String answer = "";
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> input = Stream.of(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        if (input.get(0) > input.get(1)) {
+            answer = ">";
+        }
+        if (input.get(0) < input.get(1)) {
+            answer = "<";
+        }
+        if (Objects.equals(input.get(0), input.get(1))) {
+            answer = "==";
+        }
+
+        System.out.println(answer);
+    }
 }
