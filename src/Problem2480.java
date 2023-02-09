5f2c356c7cf6e87d587fa2c53c5fda08b3a6b805
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-02-09 18:22:49 +0900

    :white_check_mark: Solve. 2480번

---
+++ src/Problem2480.java
@@ -0,0 +1,43 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Problem2480 {
+
+    public static void main(String[] args) {
+        int answer = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> input = Stream.of(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        for (int no : input) {
+            int cnt = (int) input.stream()
+                    .filter(x -> x == no)
+                    .count();
+
+            if (cnt == 3) {
+                answer = 10000 + no * 1000;
+                break;
+            }
+
+            if (cnt == 2) {
+                answer = 1000 + no * 100;
+                break;
+            }
+        }
+
+        if (answer == 0) {
+            int max_no = input.stream()
+                    .max(Comparator.comparing(x -> x))
+                    .get();
+
+            answer = max_no * 100;
+        }
+
+        System.out.println(answer);
+    }
+}
