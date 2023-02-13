1d638c07d5e82f85e99b2c6c946d105f64589e64
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2023-02-13 15:24:12 +0900

    :white_check_mark: Solve. 25192번

---
+++ src/Problem25192.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Problem25192 {
+
+    public static void main(String[] args) throws IOException {
+        int answer = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        HashSet<String> enter = new HashSet<>();
+        String input;
+
+        for (int i = 0; i < num; i++) {
+            input = br.readLine();
+
+            if (input.equals("ENTER")) {
+                enter = new HashSet<>();
+                continue;
+            }
+            if (!enter.contains(input)) {
+                enter.add(input);
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
