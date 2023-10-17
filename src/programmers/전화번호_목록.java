5382b8ded2626abf9bdebc06bd1169e4030138a0
kimjj <kimjjong321@naver.com>
2023-10-17 22:16:33 +0900

    :white_check_mark: Solve. 프로그래머스-전화번호_목록

---
+++ src/programmers/전화번호_목록.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        전화번호_목록 instance = new 전화번호_목록();
+        String[][] datas = {{"119", "97674223", "1195524421"}, {"123","456","789"}, {"12","123","1235","567","88"}};
+        Arrays.stream(datas)
+                        .forEach(data ->
+                                System.out.println(instance.solution(data)));
+    }
+}
