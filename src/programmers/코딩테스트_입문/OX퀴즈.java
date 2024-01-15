423a0d4a9efec95e508e0b35a33dc11a78e24844
Dreaming-J <kimjjong321@naver.com>
2024-01-15 16:33:07 +0900

    :white_check_mark: Solve. 프로그래머스-OX퀴즈

---
+++ src/programmers/코딩테스트_입문/OX퀴즈.java
@@ -0,0 +1,23 @@
+package programmers.코딩테스트_입문;
+
+import java.util.List;
+
+public class OX퀴즈 {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+
+        List<String> quizs = List.of(quiz);
+        for (int i = 0; i < quiz.length; i++) {
+            String[] num = quizs.get(i).split(" ");
+            int X = Integer.parseInt(num[0]);
+            int Y = Integer.parseInt(num[2]);
+            int Z = Integer.parseInt(num[4]);
+
+            if (num[1].equals("+") && X + Y == Z) answer[i] = "O";
+            else if (num[1].equals("-") && X - Y == Z) answer[i] = "O";
+            else answer[i] = "X";
+        }
+
+        return answer;
+    }
+}
